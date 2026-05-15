0648f562e114bb8ce9466939decf1a0aec9e6be4
BrandonIsma <brandontacrom@gmail.com>
2026-05-15 00:42:06 -0500

    variables cambiadas a mayusculas

---
+++ src/main/java/ec/edu/uce/proyectocentralfood/Main.java
@@ -0,0 +1,11 @@
+package ec.edu.uce.proyectocentralfood;
+
+import ec.edu.uce.proyectocentralfood.consola.MenuConsola;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MenuConsola menuConsola = new MenuConsola();
+        menuConsola.iniciar();
+    }
+}
