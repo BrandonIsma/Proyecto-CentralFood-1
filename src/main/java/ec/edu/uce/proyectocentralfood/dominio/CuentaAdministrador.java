2a53620b8981f13efc3c9ca8eb4ae6665ea28cd8
BrandonIsma <brandontacrom@gmail.com>
2026-05-15 00:07:48 -0500

    variables cambiadas a mayusculas

--- src/main/java/ec/edu/uce/proyectocentralfood/dominio/CuentaAdministrador.java
+++ src/main/java/ec/edu/uce/proyectocentralfood/dominio/CuentaAdministrador.java
@@ -2,6 +2,7 @@ package ec.edu.uce.proyectocentralfood.dominio;
 
 public class CuentaAdministrador {
 
+    // ATRIBUTO
     private String codigoAdministrador;
 
     // CONSTRUCTOR VACÍO
@@ -26,60 +27,62 @@ public class CuentaAdministrador {
     // MÉTODOS
 
     public void crearCategoria() {
-        System.out.println("Categoría creada");
+
     }
 
     public void actualizarCategoria() {
-        System.out.println("Categoría actualizada");
+
     }
 
     public void eliminarCategoria() {
-        System.out.println("Categoría eliminada");
+
     }
 
     public void crearPuntoReferencia() {
-        System.out.println("Punto de referencia creado");
+
     }
 
     public void actualizarPuntoReferencia() {
-        System.out.println("Punto de referencia actualizado");
+
     }
 
     public void eliminarPuntoReferencia() {
-        System.out.println("Punto de referencia eliminado");
+
     }
 
     public void crearLocal() {
-        System.out.println("Local creado");
+
     }
 
     public void actualizarLocal() {
-        System.out.println("Local actualizado");
+
     }
 
     public void eliminarLocal() {
-        System.out.println("Local eliminado");
+
     }
 
     public void crearPlato() {
-        System.out.println("Plato creado");
+
     }
 
     public void actualizarPlato() {
-        System.out.println("Plato actualizado");
+
     }
 
     public void eliminarPlato() {
-        System.out.println("Plato eliminado");
+
     }
 
     public void moderarResena() {
-        System.out.println("Reseña moderada");
+
     }
 
     public void gestionarUsuario() {
-        System.out.println("Usuario gestionado");
+
     }
+
+    // TOSTRING
 
     @Override
     public String toString() {
