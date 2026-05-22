b330d7ed6aefafc4a68d70399ab0a5b8554eb93d
BrandonIsma <brandontacrom@gmail.com>
2026-05-22 02:31:43 -0500

    variables cambiadas a mayusculas

---
+++ src/test/java/ec/edu/uce/proyectocentralfood/dominio/CategoriaGastronomicaTest.java
@@ -0,0 +1,52 @@
+package ec.edu.uce.proyectocentralfood.dominio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoriaGastronomicaTest {
+
+    @Test
+    void getIdCategoria() {
+    }
+
+    @Test
+    void setIdCategoria() {
+    }
+
+    @Test
+    void getNombreCategoria() {
+    }
+
+    @Test
+    void setNombreCategoria() {
+    }
+
+    @Test
+    void getDescripcion() {
+    }
+
+    @Test
+    void setDescripcion() {
+    }
+
+    @Test
+    void getPlatos() {
+    }
+
+    @Test
+    void setPlatos() {
+    }
+
+    @Test
+    void getLocales() {
+    }
+
+    @Test
+    void setLocales() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/ec/edu/uce/proyectocentralfood/dominio/CentroComidaTest.java
@@ -0,0 +1,40 @@
+package ec.edu.uce.proyectocentralfood.dominio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CentroComidaTest {
+
+    @Test
+    void getNombre() {
+    }
+
+    @Test
+    void getCuentas() {
+    }
+
+    @Test
+    void setCuentas() {
+    }
+
+    @Test
+    void setLocales() {
+    }
+
+    @Test
+    void setCategorias() {
+    }
+
+    @Test
+    void crearCategoria() {
+    }
+
+    @Test
+    void actualizarCategoria() {
+    }
+
+    @Test
+    void crearFacultad() {
+    }
+}
